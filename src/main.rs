65671c9698186ea725b776700d47d407f8c68fc7
Mizuki Sato <mizki.sato.0025@gmail.com>
2021-05-16 22:43:05 +0900

    指定した期間の日付のうち素数となる日付をプリント

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use chrono::prelude::*;
-use chrono::Date;
+use chrono::{Date, Duration};
+use primes::is_prime;
 
-pub fn date_to_int(date: Date<Utc>) -> u32 {
+pub fn date_to_int(date: Date<Utc>) -> u64 {
     let str_date = date.format("%Y%m%d").to_string();
-    str_date.parse::<u32>().unwrap()
+    str_date.parse::<u64>().unwrap()
 }
 
 fn main() {
-    let dt = Utc.ymd(2020, 1, 1);
-    println!("{}", date_to_int(dt));
+    let mut dt = Utc.ymd(2021, 1, 1);
+    while dt <= Utc.ymd(2023, 12, 21) {
+        if is_prime(date_to_int(dt)) {
+            println!("{}", dt.format("%Y-%m-%d").to_string());
+        }
+        dt = dt + Duration::days(1);
+    }
 }
 
 #[cfg(test)]
