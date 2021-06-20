b4ab6b676aaa0b7e2a8bae56cfce4f70fa25cf48
Mizuki Sato <mizki.sato.0025@gmail.com>
2021-06-20 18:44:31 +0900

    指定した期間の素数の日付を返すようにした

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
-use chrono::prelude::*;
-use chrono::{Date, Duration};
+use chrono::naive::NaiveDate;
+use chrono::Duration;
 use primes::is_prime;
+use structopt::StructOpt;
 
-pub fn date_to_int(date: Date<Utc>) -> u64 {
+#[derive(StructOpt)]
+#[structopt(name = "period")]
+struct Period {
+    #[structopt(name = "FROM")]
+    from: String,
+    #[structopt(name = "TO")]
+    to: String,
+}
+
+pub fn date_to_int(date: NaiveDate) -> u64 {
     let str_date = date.format("%Y%m%d").to_string();
     str_date.parse::<u64>().unwrap()
 }
 
+pub fn string_to_date(str_date: &str) -> NaiveDate {
+    NaiveDate::parse_from_str(str_date, "%Y-%m-%d")
+        .ok()
+        .unwrap()
+}
+
 fn main() {
-    let mut dt = Utc.ymd(2021, 1, 1);
-    while dt <= Utc.ymd(2023, 12, 21) {
+    let period = Period::from_args();
+    let from_date = string_to_date(&period.from);
+    let to_date = string_to_date(&period.to);
+    let mut dt = from_date;
+
+    while dt <= to_date {
         if is_prime(date_to_int(dt)) {
             println!("{}", dt.format("%Y-%m-%d").to_string());
         }
@@ -22,6 +42,6 @@ mod tests {
     use super::*;
     #[test]
     fn test_date_to_int() {
-        assert_eq!(date_to_int(Utc.ymd(2020, 1, 1)), 20200101);
+        assert_eq!(date_to_int(NaiveDate::from_ymd(2020, 1, 1)), 20200101);
     }
 }
