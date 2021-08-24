6f95ccfe4c98a8f536329f805d6e80ed8ac0cb57
Mizuki Sato <mizki.sato.0025@gmail.com>
2021-08-25 00:40:30 +0900

    エラトステネスのふるい実装

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use chrono::naive::NaiveDate;
 use chrono::Duration;
 use primes::is_prime;
+use rstest::rstest;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -23,6 +24,30 @@ pub fn string_to_date(str_date: &str) -> NaiveDate {
         .unwrap()
 }
 
+pub fn list_prime_numbers(number: u64) -> Vec<u64> {
+    // 参考
+    // https://qiita.com/fantm21/items/5e270dce9f4f1d963c1e
+    if number < 2 {
+        panic!("number should more than 2")
+    }
+    if number == 2 {
+        return vec![2];
+    }
+    let mut primes: Vec<u64> = vec![2];
+    let limit = (number as f64).sqrt().round() + 1f64;
+    let mut data: Vec<u64> = (3u64..number + 1).filter(|i| i % 2 != 0).collect();
+    loop {
+        let p: u64 = data[0];
+        let casted = p as f64;
+        if limit <= casted {
+            primes.append(&mut data);
+            return primes;
+        }
+        primes.push(p);
+        data = data.into_iter().filter(|e| e % p != 0).collect();
+    }
+}
+
 fn main() {
     let period = Period::from_args();
     let from_date = string_to_date(&period.from);
@@ -43,5 +68,21 @@ mod tests {
     #[test]
     fn test_date_to_int() {
         assert_eq!(date_to_int(NaiveDate::from_ymd(2020, 1, 1)), 20200101);
+    }
+
+    #[rstest(number, expected,
+        case(10, vec![2, 3, 5, 7]),
+        case(9, vec![2, 3, 5, 7]),
+        case(3, vec![2, 3]),
+        case(5, vec![2, 3, 5]),
+        case(18, vec![2, 3, 5, 7, 11, 13, 17]),
+        case(2, vec![2]),
+        #[should_panic]
+        case(1, vec![]),
+    )]
+    fn test_list_prime_numbers(number: u64, expected: Vec<u64>) {
+        // 並列化
+        // https://caddi.tech/archives/1849
+        assert_eq!(list_prime_numbers(number), expected);
     }
 }
